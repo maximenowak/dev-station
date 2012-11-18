69f880f76deb2f4830869493ee9ff5e52a9a5b0d
Maxime Nowak <maxime.nowak@gmail.com>
2012-11-18 17:00:08 +0100

    Removing the change directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,17 +11,13 @@ export PATH=$PATH:$HOME/bin
 PREV_DIR=$PWD
 WORK_DIR=/tmp/dev-station-`date +%s`
 mkdir -p $WORK_DIR
-cd $WORK_DIR
+#cd $WORK_DIR
 
 
 log "Checking for RVM..."
 if [ ! -d ~/.rvm ]; then
   log "RVM not found, installing..."
   curl -L https://get.rvm.io | bash -s stable
-
-  log "Appending RVM function to your .bash_profile..."
-  grep "scripts/rvm" ~/.bash_profile >/dev/null 2>&1 || echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
-  . ~/.rvm/scripts/rvm
 fi
 
 if [[ ! $(rvm list | grep -F 1.9.2) ]]; then
